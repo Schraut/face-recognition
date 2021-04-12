4b8b5c272ff3ddc76f077a39d4cb299570044bea
Dan Shrout <danshrout1@gmail.com>
2021-04-12 08:55:40 -0700

    installed tachyons for styling, installed react-parallax-tilt that add animation to the logo, created logo and navigation components.

---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// CSS
+//import './navigation.styles.css';
+
+const Navigation = () => {
+  return (
+    <nav
+      style={{ display: 'flex', justifyContent: 'flex-end', cursor: 'pointer' }}
+    >
+      <p>Sign Out</p>
+    </nav>
+  );
+};
+
+export default Navigation;
